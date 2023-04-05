5bbc08c7c78fab96c7838dae7e56e6b8c48b4f69
Myrsini Ioannou <mersene.io@gmail.com>
2023-04-05 15:11:31 +0300

    update controller

---
+++ genTabEx/modules/controller.py
@@ -0,0 +1,32 @@
+
+# Initialization ******Connection with View/Input Events
+
+
+# Rendering - ******Connection with View/Input Events
+
+
+# Serials
+
+################ Actions  ################
+
+# Evolve
+    # Ascend / Descend
+    # Per measure, row, page
+
+
+
+
+################ Templates  ################
+# 1. Cover with title, subtitle, author, etc..
+# 2. 3 blank pages
+# 3.    Title page
+# 4. Reverse title page (Back of title page)
+# 5. In-detail-title-page
+# 6. single blank page
+# 7. dedication (if there is one)
+# 8. single blank page
+# 9. preface (always on the right)
+# 10. body
+# 11.  2-3 single blank pages (depending on body’s end)
+# 12.  back cover
+
