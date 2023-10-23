64cd1917d750e82365c62b546dfdb194098aa3b4
Myrsini Ioannou <you@example.com>
2023-10-23 12:57:08 +0300

    removing unnecessary files

--- genTabEx/modules/model/main.py
+++
@@ -1,9 +0,0 @@
-from domain_model import *
-from evolutions import *
-from format import *
-from render import *
-from serial_note_numbers import *
-
-
-if __name__ == '__main__':
-    print("run from main file in model directory")
